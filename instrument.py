9f09aef492aa0d29beea4790286d5c0733e7d62a
Venkat Arun <venkatarun95@gmail.com>
2015-11-08 04:19:45 +0530

    With harmonics and formants

---
+++ instrument.py
@@ -0,0 +1,43 @@
+import scipy.fftpack
+import numpy as np
+
+class Instrument:
+    def __init__(self, data_filename):
+        file = open(data_filename, 'r')
+        self.filter = []
+        max_y = 0
+        for line in file.readlines():
+            x, y = line.split(' ')
+            y = 10.0 ** (float(y) / 20.0)
+            self.filter.append((float(x), y))
+            max_y = max(max_y, y)
+
+        for i in range(len(self.filter)):
+            self.filter[i] = (self.filter[i][0], self.filter[i][1] / max_y)
+
+    def apply_filter(self, sound, time_unit):
+        ''' Apply filter to add formant effect to the given sound trail
+
+        sound -- 2D array of variables representing sound amplitude
+        time_unit - number representing the number of units of time
+            between two elements in the array'''
+
+        sound = np.array(sound)
+        spectrum = scipy.fftpack.rfft(sound)
+        temp_freqs = scipy.fftpack.rfftfreq(len(sound), time_unit)
+
+        prev, next, filter_pos = (0.0, 0.0), self.filter[0], 0
+        for i in range(len(sound)):
+            freq = temp_freqs[i]
+
+            while next[0] < freq:
+                filter_pos += 1
+                prev, next = next, self.filter[filter_pos]
+
+            scale = ((freq - prev[0]) * next[1] + (next[0] - freq) * prev[1]) \
+                / (next[0] - prev[0])
+            spectrum[i] *= scale
+
+
+        sound = scipy.fftpack.irfft(spectrum)
+        return sound
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # import pyaudio
 import array
 import math
-import numpy as np
 import re
-import scipy.fftpack
 import sys
 import wave
 
+from instrument import Instrument
 
 def read_music_file(filename):
     res = [(0.0, None)]
@@ -20,6 +19,7 @@ def read_music_file(filename):
         except ValueError:
             y = None
         res.append((x, y))
+    in_file.close()
 
     if res[1][0] == 0.0:
         res = res[1:]
@@ -32,61 +32,43 @@ def note_freq(note):
 
 def generate_music(music, out_filename):
     frame_rate = 32000
+    num_harmonics = 16
+    pitch_change_factor = 3
 
     time = 0.0 # in sec
     music_pos = 0
-    phase = 0.0 # in radians
+    phase = [0.0 for i in range(num_harmonics+1)] # in radians
     sound = []
 
     while True:
         freq = music[music_pos][1]
 
         if freq != None:
-            # n = note_number(freq)
-            # if abs(freq - note_freq(n)) < abs(freq - note_freq(n+1)):
-            #     freq = note_freq(n)
-            # else:
-            #     freq = note_freq(n+1)
-            sound.append(math.sin(time * 2 * math.pi * freq + phase))
+            x = 0.0
+            for j in range(1, num_harmonics + 1):
+                if j % 2 == 0:
+                    continue
+                x += 0.2*math.sin(time * 2 * math.pi * freq * \
+                    (j+pitch_change_factor) + phase[j]) / j
+            sound.append(x)
         else:
             sound.append(0)
-        print time, freq
 
         time += 1.0 / frame_rate
-        if music[music_pos][0] < time: # and (time > prev_time + 0.1 or freq == None):
+        if music[music_pos][0] < time:
             music_pos += 1
             if music_pos >= len(music):
                 break
             if music[music_pos][1] != None and music[music_pos-1][1] != None:
-                phase += 2 * math.pi * time * \
-                    (music[music_pos - 1][1] - music[music_pos][1])
-            # prev_time = time
-            # while music_pos < len(music) and music[music_pos][0] < time:
-            #     music_pos += 1
+                for j in range(1, num_harmonics + 1):
+                    phase[j] += (j + pitch_change_factor) * 2 * math.pi * time * \
+                        (music[music_pos - 1][1] - music[music_pos][1])
 
-
-    # Filter out the high frequency noise
-    # freq_cutoff = 1000.0 # Hz
-    #
-    # sound = np.array(sound)
-    # spectrum = scipy.fftpack.rfft(sound)
-    # temp_freqs = scipy.fftpack.rfftfreq(len(sound), 1.0 / frame_rate)
-    #
-    # tmp = spectrum[0]
-    # spectrum[0] = 0
-    # cutoff_mag = spectrum.max() / 10
-    # spectrum[0] = tmp
-    #
-    # print spectrum.max()
-    # for i in range(len(sound)):
-    #     # print spectrum[i]
-    #     if temp_freqs[i] > freq_cutoff: # or abs(spectrum[i]) < cutoff_mag:
-    #         spectrum[i] = 0
-    # sound = scipy.fftpack.irfft(spectrum)
-
-    sample_width = 1
+    sound = violin.apply_filter(sound, 1.0 / frame_rate)
 
     # Write to array
+    sample_width = 1
+
     wf = wave.open(out_filename, 'wb')
     wf.setnchannels(1)
     wf.setsampwidth(sample_width)
@@ -95,7 +77,6 @@ def generate_music(music, out_filename):
     data = array.array('B')
     assert(data.itemsize == sample_width)
 
-    print len(sound)
     for x in sound:
         x = min(1.0, max(-1.0, x))
         # print x, ((2**16 - 1) * (x + 1.0))
@@ -109,5 +90,6 @@ def generate_music(music, out_filename):
 
 if __name__ == "__main__":
     assert(len(sys.argv) == 3)
+    violin = Instrument('violin.dat')
     music = read_music_file(sys.argv[1])
     generate_music(music, sys.argv[2])
